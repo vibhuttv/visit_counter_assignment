68c03fc3a8a99a54c7e4c30d565719c85b407f7d
Vibhu Khullar <vibhukhullar@Vibhus-Laptop.local>
2025-03-01 22:39:16 +0530

    Task 4

--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -13,9 +13,36 @@ from app.logging import logger
 class VisitCounterService():
     def __init__(self):
         """Initialize the visit counter service with Redis manager"""
-        # * Key:page_id     Value: (visits, time_when_fetched)
-        self.page_visits = dict()
+        # * Key:page_id     Value: [visits, time_when_fetched]
+        self.last_flushed = 0
+        self.page_visits = {}
+        self.buffer_write = {}
         self.redis_manager = RedisManager()
+        # asyncio.create_task(self.flush_visits())
+
+        asyncio.create_task(self.periodic_flush())
+        
+    async def flush_visits(self):
+        logger.info("Flushing...")
+        buffer_list = self.buffer_write.items()
+        self.buffer_write = {}
+
+        self.last_flushed = time.time()
+        
+        for cur_page, buffer_visits in buffer_list:
+            await self.redis_manager.increment(cur_page, buffer_visits)
+            
+            
+    async def periodic_flush(self):
+        while True:
+            await asyncio.sleep(30)
+            if self.buffer_write:
+                try:
+                    await self.flush_visits()
+                except Exception as e:
+                    logger.error(f"Flush error: {e}")
+
+        
 
     async def increment_visit(self, page_id: str) -> None:
         """
@@ -25,7 +52,19 @@ class VisitCounterService():
             page_id: Unique identifier for the page
         """
         # TODO: Implement visit count increment
-        await self.redis_manager.increment(page_id)
+        # if(page_id in self.buffer_write):    self.buffer_write[page_id] += 1
+        # else:
+        #     self.buffer_write[page_id] = 1
+        
+        self.buffer_write[page_id] = self.buffer_write.get(page_id, 0) + 1
+        
+        if(page_id in self.page_visits):    self.page_visits[page_id][0] += 1
+        else:   self.page_visits[page_id] = [1, 0]
+        
+        # if(time.time() - self.last_flushed > 30):   pass #self.flush_visits()
+            
+                
+                
 
     async def get_visit_count(self, page_id: str) -> int:
         """
@@ -42,10 +81,15 @@ class VisitCounterService():
         if(page_id in self.page_visits.keys()):
             time_diff = time.time() - self.page_visits[page_id][1]
             
-            if(time_diff <= 5):  
+            if(time_diff <= 30):  
+                logger.info("Fetched from memory")
                 return (self.page_visits[page_id][0], "in_memory")
             
-            
+                
+        await self.flush_visits()
+        logger.info("Fetched from redis")
+        
         visits = await self.redis_manager.get(page_id)
-        self.page_visits[page_id] = (visits, time.time())
+        logger.info(visits)
+        self.page_visits[page_id] = [int(visits), time.time()]
         return (visits, "redis")
